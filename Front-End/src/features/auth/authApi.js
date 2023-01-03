739afda799758bae50952a1eb88a9e36f2660fa4
Md Imran Hossen <freelancerimran81@gmail.com>
2023-01-03 11:32:53 +0600

    update

--- Front-End/src/features/auth/authApi.js
+++ Front-End/src/features/auth/authApi.js
@@ -2,7 +2,9 @@ import apiSlice from "../api/apiSlice";
 import { getUser } from "./authSlice";
 
 
+
 const authApi = apiSlice.injectEndpoints({
+    
     endpoints: (builder) => ({
         register: builder.mutation({
             query:(data) =>({
