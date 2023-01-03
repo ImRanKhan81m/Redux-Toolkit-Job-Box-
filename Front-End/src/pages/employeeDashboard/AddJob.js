7161a0c6bd139581fbd743346908abd8ea9f8290
Md Imran Hossen <freelancerimran81@gmail.com>
2023-01-03 20:00:36 +0600

    job post done

--- Front-End/src/pages/employeeDashboard/AddJob.js
+++ Front-End/src/pages/employeeDashboard/AddJob.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { FiTrash } from "react-icons/fi";
+import { useSelector } from "react-redux";
+import { usePostJobMutation } from "../../features/job/jobApi";
 
 const AddJob = () => {
-  const { handleSubmit, register, control } = useForm();
+  const {user:{companyName}} = useSelector((state) => state.auth)
+  const { handleSubmit, register, control } = useForm({
+    defaultValues: {
+      companyName
+    }
+  });
+  const [postJob, {isLoading, isError, error}] = usePostJobMutation()
   const {
     fields: resFields,
     append: resAppend,
@@ -22,6 +30,7 @@ const AddJob = () => {
 
   const onSubmit = (data) => {
     console.log(data);
+    postJob(data)
   };
 
   return (
